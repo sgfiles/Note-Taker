f0c70b8397c4d1bd5b117f87e32618c1ee15cbfd
Sheralyn Files <gired0329@yahoo.com>
2021-10-21 22:25:57 -0400

    initial commit

---
+++ public/routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+
+router.get('/', (req, res) => {
+  
+})
+
+
+
+router.post('/notes', (req, res) => {
+  
+})
+
+module.exports = router;
