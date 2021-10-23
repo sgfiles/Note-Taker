61ce4861613fdb8e32f2016cfe170d29ff0b1ea9
Sheralyn Files <gired0329@yahoo.com>
2021-10-22 22:24:21 -0400

    added helpers and routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const fs = require('fs')
 const router = require('express').Router();
+const { text } = require('express');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const uuid  = require('../helpers/uuid');
 
+//Get for retrieving notes
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
 
-
-router.get('/', (req, res) => {
-  
-})
-
-
-
+// POSt to submit a not
 router.post('/notes', (req, res) => {
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, text, id: uuid(),
+        };
+    }
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added');
+     
+    
   
 })
 
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const fs = require('fs')
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes')
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-
 app.use('/api',apiRoutes)
 app.use('/', htmlRoutes);
 
